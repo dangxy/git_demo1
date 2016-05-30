4d390e057ef5e58aa6611a7ad156c40dc3014dab
dangxueyi <dangxy99@163.com>
2016-05-30 11:22:56 +0800

    1.添加点击事件

--- app/src/main/java/com/example/dangxueyi/git_demo1/MainActivity.java
+++ app/src/main/java/com/example/dangxueyi/git_demo1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.dangxueyi.git_demo1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.bt_start)
+    Button btStart;
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mContext=this;
+
+    }
+
+    @OnClick(R.id.bt_start)
+    public void onClick() {
+
+        Toast.makeText(mContext,"按钮点击",Toast.LENGTH_LONG).show();
     }
 }
